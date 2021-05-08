c486187cfcbae04a8c9b537e1e5ff5bfbfa7a9f9
Ordinateur-Hack <dominic-plein@gmx.de>
2021-05-09 01:21:02 +0200

    Replace conditional logic on price code with polymorphism

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -23,4 +23,27 @@ public class Movie {
         return title;
     };
 
+    double getCharge(int daysRented) {
+        // 🔀 Refactoring: Replace conditional logic on price code with polymorphism
+        // 🔀 Refactoring: Extract method to object whose data the method uses
+        // 🔀 Refactoring: Rename variables for better readability
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
 }
